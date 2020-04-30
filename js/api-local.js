db806eea49ca6719dc012fd770430b5fe2676cfc
honghan <honghan.wu@gmail.com>
2020-04-30 13:53:07 +0100

    updated

--- js/api-local.js
+++ js/api-local.js
@@ -6,6 +6,7 @@ if (typeof qbb == "undefined"){
 	if(typeof qbb.inf == "undefined") {
 
 		qbb.inf = {
+			service_url: "https://honghan.info/semehr_ws/api",
 			key: "covid-free-key",
 
 			localModels: null,
@@ -52,6 +53,59 @@ if (typeof qbb == "undefined"){
 						}
 					}
 				});
+
+				var apiName = "predict";
+				var sendObject={
+					r:apiName,
+					key: qbb.inf.key,
+					dataset: dataset,
+					modelId: modelId,
+					caseDetails: caseDetails
+				};
+				qbb.inf.callAPI(sendObject, function (s) {
+					// do nothing
+				});
+			},
+
+			callAPI: function(sendObject, cb){
+				qbb.inf.ajax.doPost(sendObject, function(s){
+					var ret = s;
+					if (ret && ret.status == "200" && ret.data)
+					{
+						if (typeof cb == 'function')
+							cb(ret.data);
+					}else
+					{
+						if (typeof cb == 'function')
+							cb();
+					}
+				}, function(){
+					if (typeof checkOutDataCB == 'function')checkOutDataCB();
+				});
+			},
+
+			ajax: {
+				doGet:function(sendData,success,error){
+					qbb.inf.ajax.doSend("Get",null,sendData,success,error);
+				},
+				doPost:function(sendData,success,error){
+					qbb.inf.ajax.doSend("Post",null,sendData,success,error);
+				},
+				doSend:function(method,url,sendData,success,error){
+					dataSuccess = function(data){
+						(success)(eval(data));
+					};
+					if (sendData) sendData.token = "";
+					jQuery.ajax({
+						type: method || "Get",
+						url: url || qbb.inf.service_url,
+						data: sendData || [],
+						cache: false,
+						dataType: "jsonp", /* use "html" for HTML, use "json" for non-HTML */
+						success: dataSuccess /* (data, textStatus, jqXHR) */ || null,
+						error: error /* (jqXHR, textStatus, errorThrown) */ || null
+					});
+				}
 			}
 		};
 	}
